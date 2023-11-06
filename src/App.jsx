782225fbab967c35dcde54c59fd19f335c9aa455
Trần Quang Trí <tranwangtristudy@gmail.com>
2023-11-06 16:17:53 +0700

    Updated and Stopped

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar />
       </section>
-      <section>Parallax</section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
-      <section>Parallax</section>
+      <section id="Portfolio">Parallax</section>
       <section>Portfolio1</section>
       <section>Portfotio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 }
--- src/components/Sidebar/links/Links.jsx
+++ src/components/Sidebar/links/Links.jsx
@@ -1,5 +1,37 @@
+// motion
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.1 },
+  },
+  close: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const itemsVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  close: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 const Links = () => {
-  return <div>Links</div>;
+  const pages = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+  return (
+    <motion.div className="links" variants={variants}>
+      {pages.map((item) => (
+        <motion.a href={`#${item}`} key={item} variants={itemsVariants}>
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
 };
 
 export default Links;
